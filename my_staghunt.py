d4af14a579bdc4c6ee387517118d1dee6cb501a4
kathyyliu <kliu4@oxy.edu>
2021-03-16 13:25:45 -0700

    Add funcs to run one step. Unresolved error in agent file

--- my_staghunt.py
+++ my_staghunt.py
@@ -1,20 +1,21 @@
-from pythonian import Pythonian
-import pyhop    # for operators?
-import run_sim
+from companionsKQML.pythonian import Pythonian
+import socialSim.run_sim as sim
+import logging
+
+logger = logging.getLogger('StagHuntAgent')
 
 class StagHuntAgent(Pythonian):
 
     name = "StagHuntAgent"
 
     def __init__(self, **kwargs):
-        super().__init__(**kwargs)  # TODO: lookup
-        for operator in staghunt_htn operators.items(): # TODO: how to get operators?
-            self.add_achieve(operator[0], operator[1])
+        super(StagHuntAgent, self).__init__(**kwargs)
+        self.add_achieve(sim.my_make_game, 'my_make_game')
+        self.add_achieve(sim.my_run_one, 'my_run_one')
 
 
 if __name__ == "__main__":
-    a = StagHuntAgent(host='localhost', port=9000, localPort=8950, debug=True)
-    # AGENT = TestAgent.parse_command_line_args()
+    logger.setLevel(logging.DEBUG)
+    AGENT = StagHuntAgent.parse_command_line_args() # kqml AssertionError: Connection formed but output (None) not set.
 
-# (achieve :receiver TestAgent :content (task :action (test_achieve data)))
-# test_acheive is an achieve, but also method defined in same file--does it matter?
+# (achieve :receiver StagHuntAgent :content (task :action (run_sim (2, 1, 3))))
